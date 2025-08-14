9f3ca766630e3a090de8cf90904f606089a9cfae
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-08-14 08:31:32 -0300

    fix: adjust

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
-import { ProductsResolver } from './products/products.resolver';
-import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [AuthModule, ProductsModule],
+  imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService, ProductsResolver],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
